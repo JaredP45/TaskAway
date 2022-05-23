e48f17d7203584afdb22dc70eabe5bd77ab59068
JaredP45 <52184578+JaredP45@users.noreply.github.com>
2022-05-23 16:20:38 -0500

    * Create database * Update gitignore * Update main file * Update models file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+
+from .models import Todo
+from .database import (
+    create_todo,
+    fetch_all_todos,
+    update_todo,
+    fetch_one_todo,
+    remove_todo,
+)
 
 app = FastAPI()
 
@@ -18,26 +27,39 @@ app.add_middleware(
 )
 
 
-@app.get("/api/todo")
+@app.get("/api/todo/")
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
 
-@app.get("/api/todo")
-async def post_todo(todo):
-    return 1
+@app.get("/api/todo/{title}/", response_model=Todo)
+async def get_todo_by_title(title):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no task with the title {title}.")
 
 
-@app.get("/api/todo{id}")
-async def put_todo(id, data):
-    return 1
+@app.post("/api/todo/", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Something went wrong.")
 
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id(id):
-    return 1
+@app.put("/api/todo/{title}/", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no taks with the title {title}.")
 
 
-@app.get("/api/todo{id}")
-async def delete_todo(id):
-    return 1
+@app.delete("/api/todo/{title}/")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return "Successfully deleted todo."
+    raise HTTPException(404, f"There is no task with the tite {title}.")
