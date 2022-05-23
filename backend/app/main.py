e12ba3b917f3085a21fc59d987503cf35b017544
JaredP45 <52184578+JaredP45@users.noreply.github.com>
2022-05-23 15:24:57 -0500

    Update main with CRUD functions, and create model file.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,43 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/todo")
+async def get_todo():
+    return 1
+
+
+@app.get("/api/todo")
+async def post_todo(todo):
+    return 1
+
+
+@app.get("/api/todo{id}")
+async def put_todo(id, data):
+    return 1
+
+
+@app.get("/api/todo{id}")
+async def get_todo_by_id(id):
+    return 1
+
+
+@app.get("/api/todo{id}")
+async def delete_todo(id):
+    return 1
