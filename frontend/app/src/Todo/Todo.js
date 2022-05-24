fa0323332a2666ab334fec944c89061f64cb139e
JaredP45 <52184578+JaredP45@users.noreply.github.com>
2022-05-23 20:15:47 -0500

    First iteration of edit button option.

--- frontend/app/src/Todo/Todo.js
+++ frontend/app/src/Todo/Todo.js
@@ -1,10 +1,23 @@
 // Module Imports
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Todo(props) {
+	const [title, setTitle] = useState('');
+	const [desc, setDesc] = useState('');
+    const [isEditable, setIsEditable] = useState(false);
+
     const handleDeleteTodo = (title) => {
         axios.delete(`http://localhost:8000/api/todo/${title}`)
+            .then(res => console.log(res))
+    };
+
+    const handleEditTodo = () => {
+        setIsEditable(!isEditable);
+    };
+
+    const handleUpdateTodo = () => {
+        axios.put(`http://localhost:8000/api/todo/${title}`, {'title': title, 'description': desc})
             .then(res => console.log(res))
     };
 
@@ -12,11 +25,39 @@ export default function Todo(props) {
         <div style={{ display: 'flex', justifyContent: 'center'}}>
             <div style={{ width: '500px' }}>
                 <p>
-                    <span style={{ fontWeight: 'bold' }}>
-                        { props.todo.title }:
-                    </span>
+                    { (!isEditable)
+                        ?
+                            <div>
+                                <span style={{ fontWeight: 'bold' }}>
+                                    { props.todo.title }
+                                </span>
+                                { props.todo.description }
+                            </div>
+                        :
+                            <div>
+                                <input 
+                                    onChange={(event) => setTitle(event.target.value)}
+                                    placeholder='Title'
+                                />
+                                <input 
+                                    onChange={event => setDesc(event.target.value)} 
+                                    placeholder='Description'
+                                />
+                                <button 
+                                    onClick={() => handleUpdateTodo(props.todo.title)}
+                                    style={{ color: 'blue'}}
+                                >
+                                    Edit
+                                </button>
+                            </div>
+                    }
 
-                    { props.todo.description }
+                    <button 
+                        onClick={handleEditTodo}
+                        style={{ color: 'green'}}
+                    >
+                        Edit
+                    </button>
 
                     <button 
                         onClick={() => handleDeleteTodo(props.todo.title)}
@@ -24,7 +65,6 @@ export default function Todo(props) {
                     >
                         Delete
                     </button>
-                
                 </p>
             </div>
         </div>
